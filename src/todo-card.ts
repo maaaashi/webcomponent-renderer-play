18c32bc87cb17e697ad3c74d6984252be03392dd
maaaashi <doyaseizin.7@gmail.com>
2024-12-27 23:24:30 +0900

    wip

--- src/todo-card.ts
+++ src/todo-card.ts
@@ -9,6 +9,14 @@ export class TodoCard extends LitElement {
   @property({ type: String})
   title = this.todoId.toString()
 
+  updated(changedProperties: { has: (arg: string) => any; }) {
+    if (changedProperties.has('todoId')) {
+      // `todoId` が変更された場合に `title` を更新
+      this.title = this.todoId.toString();
+    }
+  }
+
+
   render() {
     return html`
       <div>
